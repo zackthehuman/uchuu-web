403d1dc7086af083b6e0500bafd9e794bec9986e
zackhehuman <zack@zackthehuman.com>
2013-11-08 09:19:51 -0800

    Split tileset into controller/view, using delegation

--- assets/js/uchuu.js
+++ assets/js/uchuu.js
@@ -261,7 +261,12 @@ define('uchuu', function() {
         // .append("<h1>Done!</h1><h2>" + stage.tileset + "</h2>")
         // .append(uchuu.textures[uchuu.tilesets[stage.tileset].surface])
       
+      var tilesetController = new TilesetController({
+        delegate: this
+      });
+
       var tilesetView = new TilesetView({
+        delegate: tilesetController,
         tileset: uchuu.tilesets[stage.tileset],
         tilesetImage: uchuu.textures[uchuu.tilesets[stage.tileset].surface]
       });
@@ -709,18 +714,54 @@ define('uchuu', function() {
     }
   });
 
+  var TilesetController = Backbone.View.extend({
+    selectedTileIndex: null,
+
+    onTileClicked: function(sourceView, evt) {
+      var tileIndex;
+
+      if(evt) {
+        tileIndex = $(evt.currentTarget).data('tile-index');
+
+        if(sourceView && sourceView.selectTileByIndex) {
+          sourceView.selectTileByIndex(tileIndex);
+        }
+
+        if(tileIndex !== this.selectedTileIndex) {
+          this.selectedTileIndex = tileIndex;
+          this.trigger('selectedTileChanged', this.selectedTileIndex);
+        }
+      }
+    }
+  });
+
   var TilesetView = Backbone.View.extend({
+    className: 'tileset-container',
+
     delegate: null,
     
     events: {
-      'click .tile': '_handleTileClick'
+      'click .tile': '_handleTileClick',
+      'mousedown': '_handleContainerMousedown',
+      'mouseup': '_handleContainerMouseup',
+      'mousemove': '_handleContainerMousemove'
     },
 
     tileset: null,
 
     tilesetImage: null,
 
+    tileIndexMap: null,
+
     initialize: function(options) {
+      _.bindAll(this,
+        'render',
+        'selectTileByIndex',
+        '_handleTileClick',
+        '_handleContainerMousedown',
+        '_handleContainerMouseup',
+        '_handleContainerMousemove');
+
       if(options) {
         if(options.delegate) {
           this.delegate = options.delegate;
@@ -728,6 +769,7 @@ define('uchuu', function() {
 
         if(options.tileset) {
           this.tileset = options.tileset;
+          this.tileIndexMap = {};
         }
 
         if(options.tilesetImage) {
@@ -741,9 +783,12 @@ define('uchuu', function() {
 
       if(this.tileset && this.tilesetImage) {
         this.$el.empty();
+        delete this.tileIndexMap;
+        this.tileIndexMap = {};
 
         _.each(this.tileset.tiles, _.bind(function(tileMeta, index) {
           var tileElement = $('<div />', { 'class': 'tile' });
+
           tileElement.css({
             'background-image': 'url(/load?path=' +this.tileset.surface + ')',
             'background-position': (-tileMeta.x) + 'px ' + (-tileMeta.y + 'px'),
@@ -753,28 +798,62 @@ define('uchuu', function() {
           });
 
           tileElement.data('tile-index', index);
-
           tileElements.push(tileElement);
+
+          this.tileIndexMap['index-' + index] = tileElement;
         }, this));
 
         this.$el.append(tileElements);
-
-        this.$el.css({
-          'border': '1px solid #f00',
-          'width': '96px',
-          'overflow': 'hidden',
-          'zIndex': 10000,
-          'position': 'fixed',
-          'top': '200px',
-          'left': '20px'
-        });
       }
 
       return this;
     },
 
+    selectTileByIndex: function(index) {
+      var $tileElement;
+
+      if(this.tileIndexMap) {
+        $tileElement = this.tileIndexMap['index-' + index];
+
+        if($tileElement) {
+          $tileElement.addClass('selected').siblings().removeClass('selected');
+        }
+      }
+    },
+
     _handleTileClick: function(evt) {
       console.log('Clicked on tile #' + $(evt.currentTarget).data('tile-index'));
+
+      if(this.delegate && this.delegate.onTileClicked) {
+        this.delegate.onTileClicked(this, evt);
+      }
+
+      evt.stopPropagation();
+      evt.preventDefault();
+    },
+
+    _handleContainerMousedown: function(evt) {
+      this.movingLock = true;
+      this.mouseOffsetX = evt.clientX - this.el.offsetLeft;
+      this.mouseOffsetY = evt.clientY - this.el.offsetTop;
+    },
+
+    _handleContainerMouseup: function(evt) {
+      this.movingLock = false;
+    },
+
+    _handleContainerMousemove: function(evt) {
+      var x = evt.clientX - this.mouseOffsetX,
+        y = evt.clientY - this.mouseOffsetY;
+
+      // TODO: Apply clipping/bounds checking to keep the palette on screen
+
+      if(this.movingLock) {
+        this.$el.css({
+          top: y + 'px',
+          left: x + 'px'
+        });
+      }
     }
   });
 
